8d6f4fa74d6cdc13517d04305d92bb58de753d34
rajangdavis <rajangdavis@gmail.com>
2019-09-29 14:38:17 -0700

    modified proposal; will start building things to use the host version of python/golang dependencies, will figure out Docker stuff later

--- jq_parse_notebook.sh
+++ jq_parse_notebook.sh
@@ -1,1 +1,2 @@
-$ cat ./jupyter_proofs/fgr_v0.0.1.ipynb | jq '[.cells[] | select(.cell_type | contains("code")) | .source | join("\n")] | join("\n")'
+#!/bin/bash
+cat ./jupyter_proofs/fgr_v0.0.1.ipynb | ~/Desktop/jq-win64.exe '[.cells[] | select(.cell_type | contains("code")) | .source | join("\n")] | join("\n")'
